cd3603f14269e74cbc38c58e3104b9de053de7f0
Cecilia Wikstrom <cwikstro@katak.ibis.ulaval.ca>
2015-07-09 16:13:25 -0400

    Change in mapping script

--- 01_scripts/02_bwa_mapping.sh
+++ 01_scripts/02_bwa_mapping.sh
@@ -6,11 +6,11 @@ MAPPED="04_mapping_bwa"
 PROJECT_NAME=$(ls -1 $REFERENCE/*.fasta | sed 's/02_raw_data\///g' | sed 's/\.ensembl\.cdna.fasta//g')
 
 # Indexing reference sequences in the FASTA format
-cp $REFERENCE/*.fasta $TRIMMED
-bwa index -p $PROJECT_NAME $TRIMMED/*.fasta
+#cp $REFERENCE/*.fasta $TRIMMED
+#bwa index -p $PROJECT_NAME $TRIMMED/*.fasta
 
 # Performing the alignment (mapping)
-for file in `ls -1 $TRIMMED/*_R1.fastq | sed 's/_R1\.fastq//g'`; do
+for file in `ls -1 $TRIMMED/*_R1.paired.fastq | sed 's/_R1\.fastq//g'`; do
     bwa mem -t 10 $PROJECT_NAME $TRIMMED/${file}_R1.paired.fastq $TRIMMED/${file}_R2.paired.fastq >${file}.sam
 done
 
