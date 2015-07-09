9e382fd6afde4722ef1749a67d77d605d90c465f
Cecilia Wikstrom <cwikstro@katak.ibis.ulaval.ca>
2015-07-09 15:55:41 -0400

    Change in mapping script

--- 01_scripts/02_bwa_mapping.sh
+++ 01_scripts/02_bwa_mapping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # GLOBAL VARIABLES
-TRIMMED_READS="03_trimmed"
+TRIMMED="03_trimmed"
 REFERENCE="02_raw_data"
 MAPPED="04_mapping_bwa"
 PROJECT_NAME=$(ls -1 $REFERENCE/*.fasta | sed 's/02_raw_data\///g' | sed 's/\.ensembl\.cdna.fasta//g')
@@ -10,14 +10,14 @@ cp $REFERENCE/*.fasta $TRIMMED
 bwa index -p $PROJECT_NAME $TRIMMED/*.fasta
 
 # Performing the alignment (mapping)
-for file in `ls -1 $TRIMMED_READS/*_R1.fastq | sed 's/_R1\.fastq//g'`; do
-    bwa mem -t 10 $PROJECT_NAME $TRIMMED_READS/${file}_R1.fastq $TRIMMED_READS/${file}_R2.fastq >${file}.sam
+for file in `ls -1 $TRIMMED/*_R1.fastq | sed 's/_R1\.fastq//g'`; do
+    bwa mem -t 10 $PROJECT_NAME $TRIMMED/${file}_R1.fastq $TRIMMED/${file}_R2.fastq >${file}.sam
 done
 
 # Cleaning up
-mv $TRIMMED_READS/*.sam $MAPPED
-mv $TRIMMED_READS/*.sa $MAPPED
-mv $TRIMMED_READS/*.amb $MAPPED
-mv $TRIMMED_READS/*.ann $MAPPED
-mv $TRIMMED_READS/*.bwt $MAPPED
-mv $TRIMMED_READS/*.pac $MAPPED
+mv $TRIMMED/*.sam $MAPPED
+mv $TRIMMED/*.sa $MAPPED
+mv $TRIMMED/*.amb $MAPPED
+mv $TRIMMED/*.ann $MAPPED
+mv $TRIMMED/*.bwt $MAPPED
+mv $TRIMMED/*.pac $MAPPED
